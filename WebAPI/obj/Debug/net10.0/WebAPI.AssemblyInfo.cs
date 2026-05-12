ee8543a95240fe8f4bd6107fc188ea5fd3a23ba1
Kamonchanok Karum <kamonchanok.karum@student.upt.ro>
2026-05-12 12:57:48 +0300

    authController

--- WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdf6cdcea50b28716dafcdaf6519d8fda31296a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+819b98bda90276a66e60fb59f50bd4d383c087d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
