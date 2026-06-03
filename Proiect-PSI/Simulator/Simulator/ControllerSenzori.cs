068ade1ec679b07b1b0f3e914e97b8a95c60d6ef
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-03 20:17:26 +0300

    Adaugat schimbari buton emergency stop si reset la pagina

--- Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
+++ Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
@@ -24,13 +24,23 @@ namespace Simulator
         [HttpPost("activare/{numeSenzor}")]
         public IActionResult ActivareSenzor(string numeSenzor)
         {
-            if (numeSenzor == "S1") 
+            switch (numeSenzor)
             {
-                _stare.ApasaStartBanda(1); 
-            }
-            else if (numeSenzor == "S0") 
-            {
-                _stare.ApasaStopGeneralS0();
+                case "S1": _stare.ApasaStartBanda(1); break;
+                case "S2": _stare.ApasaStartBanda(2); break;
+                case "S3": _stare.ApasaStartBanda(3); break;
+                case "S4": _stare.ApasaStartBanda(4); break;
+                case "S6": _stare.SetPozitieClapeta(true, false, false); break;
+                case "S7": _stare.SetPozitieClapeta(false, true, false); break;
+                case "S8": _stare.SetPozitieClapeta(false, false, true); break;
+                case "S0": 
+                    if (_stare.S0) _stare.ElibereazaStopGeneralS0();
+                    else _stare.ApasaStopGeneralS0(); 
+                    break;
+                case "RESET": 
+                    _stare.ResetSistem(); 
+                    _stare.ElibereazaStopGeneralS0();
+                    break;
             }
 
             return Ok($"Comanda pentru {numeSenzor} a fost procesată.");
