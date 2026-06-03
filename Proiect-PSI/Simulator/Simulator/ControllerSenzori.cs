f3088004065a73a50c07e182a78d48a938c2514b
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-03 18:51:08 +0300

    Injectat Hub in logica ControllerSenzori.cs

--- Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
+++ Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Simulator.Hubs;
 
 namespace Simulator
 {
@@ -7,10 +9,12 @@ namespace Simulator
     public class ControllerSenzori : ControllerBase
     {
         private readonly StareSistem _stare;
+        private readonly IHubContext<M23Hub> _hubContext;
 
-        public ControllerSenzori(StareSistem stare)
+        public ControllerSenzori(StareSistem stare, IHubContext<M23Hub> hubContext)
         {
             _stare = stare;
+            _hubContext = hubContext;
         }
 
         // Interfața apelează asta ca să vadă starea tuturor senzorilor
@@ -22,7 +26,7 @@ namespace Simulator
 
         // Interfața apelează asta ca să "simuleze" activarea unui senzor (ex: apasă S1)
         [HttpPost("activare/{numeSenzor}")]
-        public IActionResult ActivareSenzor(string numeSenzor)
+        public async Task<IActionResult> ActivareSenzor(string numeSenzor)
         {
             if (numeSenzor == "S1") 
             {
@@ -32,6 +36,8 @@ namespace Simulator
             {
                 _stare.ApasaStopGeneralS0();
             }
+
+            await _hubContext.Clients.All.SendAsync("PrimesteStareNoua", _stare);
 
             return Ok($"Comanda pentru {numeSenzor} a fost procesată.");
         }
