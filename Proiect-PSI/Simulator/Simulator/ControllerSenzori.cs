89f23b4935c5e42f2917f41249a585439dd42035
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-03 19:49:29 +0300

    Scos SignalR din ConrollerSenzori

--- Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
+++ Proiect-PSI/Simulator/Simulator/ControllerSenzori.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using Simulator.Hubs;
 
 namespace Simulator
 {
@@ -9,12 +7,10 @@ namespace Simulator
     public class ControllerSenzori : ControllerBase
     {
         private readonly StareSistem _stare;
-        private readonly IHubContext<M23Hub> _hubContext;
 
-        public ControllerSenzori(StareSistem stare, IHubContext<M23Hub> hubContext)
+        public ControllerSenzori(StareSistem stare)
         {
             _stare = stare;
-            _hubContext = hubContext;
         }
 
         // Interfața apelează asta ca să vadă starea tuturor senzorilor
@@ -26,7 +22,7 @@ namespace Simulator
 
         // Interfața apelează asta ca să "simuleze" activarea unui senzor (ex: apasă S1)
         [HttpPost("activare/{numeSenzor}")]
-        public async Task<IActionResult> ActivareSenzor(string numeSenzor)
+        public IActionResult ActivareSenzor(string numeSenzor)
         {
             if (numeSenzor == "S1") 
             {
@@ -36,8 +32,6 @@ namespace Simulator
             {
                 _stare.ApasaStopGeneralS0();
             }
-
-            await _hubContext.Clients.All.SendAsync("PrimesteStareNoua", _stare);
 
             return Ok($"Comanda pentru {numeSenzor} a fost procesată.");
         }
--- Proiect-PSI/Simulator/Simulator/obj/Debug/net10.0/Simulator.AssemblyInfo.cs
+++ Proiect-PSI/Simulator/Simulator/obj/Debug/net10.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84bdf27c02e78d726af20f44a204e15f9aa18908")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23f9d1326cb0681e2370fef2aee0fdc53d822c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
