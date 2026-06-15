1e6c1e07fd51ec3b58e48a0f593a73922f8ee3db
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-15 15:28:29 +0300

    Merged branch Alex into main

--- WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb69752b8ae7b3c4f3d72758ce9f43d0b648130")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+633ffe77579aa2515e1cc2122ac36852c0decd3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
