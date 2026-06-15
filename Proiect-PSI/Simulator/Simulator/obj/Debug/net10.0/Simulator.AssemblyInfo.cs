da62f2eca170bbf731ebe88596fcba4f4d6768b5
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-15 15:27:50 +0300

    fixed page not found error

--- Proiect-PSI/Simulator/Simulator/obj/Debug/net10.0/Simulator.AssemblyInfo.cs
+++ Proiect-PSI/Simulator/Simulator/obj/Debug/net10.0/Simulator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241d56d381152aa826201eeef0d76bcebefa8ab4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841156fd4d3757c793f316e0a30505fb3fc4f3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simulator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
