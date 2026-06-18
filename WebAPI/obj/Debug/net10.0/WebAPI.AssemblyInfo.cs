46b887954e2bededa718e82f3cf164f1999ce2c0
Kamonchanok Karum <kamonchanok.karum@student.upt.ro>
2026-06-18 13:03:32 +0300

    am legat Simulator, BlazorApp cu WebAPI
    Am modificat (aka am adaugat o sectiune) putin la SIST_M23 de la BlazorApp pt a putea conecta cu WebAPI

--- WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ce74d39a70a9d83e7a3b4da7c55f56a6423ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b461b35fb939c887330be67ea0848ce5e6689f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
