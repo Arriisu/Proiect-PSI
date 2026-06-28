d7ad6a436e139879b58e5a49bd70cf84ce8f522c
Ddaerb <varga.alexandra.maria.239@gmail.com>
2026-06-28 20:22:30 +0300

    .

--- WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net10.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78ce74d39a70a9d83e7a3b4da7c55f56a6423ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3771ddfae207340bfd7d65052f0f3a64e176e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
